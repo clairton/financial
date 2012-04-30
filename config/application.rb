cde667c017db1e2a559ea9cd136fcdb1f6d7f391
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2012-04-30 19:11:17 -0300

    false

--- config/application.rb
+++ config/application.rb
@@ -51,9 +51,10 @@ module Financial
     # config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
