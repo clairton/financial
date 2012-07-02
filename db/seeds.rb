065c4dfbc7e3257ead588de9da2ab3d5efee8050
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2012-07-02 18:22:08 -0300

    Alterado seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-@user = User.new(:email => 'clairton.rodrigo@gmail.com',:password => '123456')
+@user = User.new(:email => 'admin@sample.com',:password => '123456')
 @user.save
 
 @sum = Sum.create(:id => 1, :value => 0.00)
