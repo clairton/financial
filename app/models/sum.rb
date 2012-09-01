bea07d7ce61697f8e74e8d93e4fea18ea01bc4b3
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2012-09-01 17:36:16 -0300

    Validação para nao adiocinar mais de uma soma

--- app/models/sum.rb
+++ app/models/sum.rb
@@ -1,7 +1,18 @@
 class Sum < ActiveRecord::Base	
 	attr_accessible :value
+  before_create :unique_registry
 
-	         
+	def unique_registry
+    if Sum.all.size >= 1
+      self.errors.add(:value, "Pode haver somente um registro em #{I18n.t(:sum)}")
+      return false
+    end
+  end
+
+  def custom_label
+      I18n.t(:sum)
+  end 
+
   def account
     @a = Account.new
     @a.operation= '+'
